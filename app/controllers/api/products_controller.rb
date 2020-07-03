ed580747480a24bdb5bf3194f6d9d1e60e023c9d
Dan <dcox38@gmailL.com>
2020-07-02 21:10:58 -0500

    initial index and show methods and routes with proper naming

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,8 +1,12 @@
 class Api::ProductsController < ApplicationController
-  def single_product
-    @product = Product.first
-    render 'product.json.jb'
+  def index
+    @products = Product.all 
+    render 'index.json.jb'
   end
 
+  def show
+    @product = Product.find_by(id: params[:id])
+    render 'show.json.jb'
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
-   
+    get "/products" => "products#index"
+
+    get "/products/:id" => "products#show"
+
+  end
 end
